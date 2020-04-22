8f59802298483567661085c530b3cc24ff6ab7a9
Igor Lima <igorlima1740@gmail.com>
2020-04-22 12:24:05 -0300

    arquivos plaid apagados

--- back/server.js
+++ back/server.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser")
 const passport = require("passport")
 
 const users = require("./routes/api/users")
-// const plaid = require("./routes/api/plaid")
 
 const app = express()
 
@@ -37,7 +36,6 @@ require("./config/passport")(passport)
 
 // Routes 
 app.use("/api/users", users)
-// app.use("/api/plaid", plaid)
 
 const port = process.env.PORT || 5000;
 // process.env.port is Heroku's port if you choose to deploy the app there 
